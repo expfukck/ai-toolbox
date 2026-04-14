9533ecdea488b15c5c6c17980eb816a57c0eadcd
coulsontl <149155411+coulsontl@users.noreply.github.com>
2026-04-14 23:45:43 +0800

    Revert "chore(cask): update ai-toolbox for v0.8.1_beta1"
    This reverts commit b34481cbd4aac564b1b003162f25126d92767ba8.

--- Casks/ai-toolbox.rb
+++ Casks/ai-toolbox.rb
@@ -1,14 +1,14 @@
 cask "ai-toolbox" do
-  version "0.8.1_beta1"
+  version "0.8.1"
 
   on_arm do
-    sha256 "f7e810a5633c72161a2443b0fff24eab3c3b4ed17a87a56fc777cbe94909c11b"
+    sha256 "e27b0c1355effcafceab0976166bacb828a1a28505ca334e1cb19b8a8f09e967"
     url "https://github.com/coulsontl/ai-toolbox/releases/download/v#{version}/AI.Toolbox_0.8.1_aarch64.dmg",
         verified: "github.com/coulsontl/ai-toolbox/"
   end
 
   on_intel do
-    sha256 "7adfbe98fe63c2318300732ac8ea343e5f629b0d88da1800402a7e62ca08e06b"
+    sha256 "e2ff9b691ec7ead1157fad6f6e8ae9518ec0803ecbbb38eaa565e62d48b7eced"
     url "https://github.com/coulsontl/ai-toolbox/releases/download/v#{version}/AI.Toolbox_0.8.1_x64.dmg",
         verified: "github.com/coulsontl/ai-toolbox/"
   end
